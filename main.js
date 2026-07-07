56d810f6f6feb4dc49bca5f4776a56c03457336e
wwgabigf <gabrielli.garcia.faria@escola.pr.gov.br>
2026-07-07 17:17:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,34 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = diminuiTamanho;
 
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
+    if (tamanhoSenha> 1){
+        tamanhoSenha--;
+    }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho (){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhosenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    let senha ='';
+    for (let i=0; i < tamanhoSenha; i++) {
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letraMaiusculas[numeroAleatorio];
+    }
+campoSenha.value = senha;
 }
